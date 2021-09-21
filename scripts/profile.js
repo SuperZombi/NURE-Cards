083e34bfeb79d7193f5fec082e6e63e934b7bd40
Super Zombi <rost201541@gmail.com>
2021-09-21 23:39:03 +0300

    Add files via upload

---
+++ scripts/profile.js
@@ -0,0 +1,57 @@
+server = "http://127.0.0.1:5000"
+// server = 'https://nure-cards.herokuapp.com'
+
+
+function load_args(){
+	params = window.location.href.split("?").slice(1)
+	args = {}
+	for (i = 0; i < params.length; i++) {
+		temp = params[i].split("=")
+		args[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1])
+	}
+	if (args.hasOwnProperty("user")){
+		load_user_info(args["user"])
+	}
+	else{
+		load_my_info()
+	}
+}
+function load_user_info(user){
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", `${server}/user_exists`)
+	xhr.timeout = 5000;
+	xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+	xhr.send(JSON.stringify({'name': user}))
+	xhr.onload = function() {
+		answer = JSON.parse(xhr.response)
+		if (answer.exist){
+			document.getElementById("user_name").innerHTML = user
+			document.getElementById("profile").style.display = "block"
+		}
+		else{
+			document.getElementById("not_found").style.display = "block"
+		}
+	}
+	xhr.ontimeout = function() {
+		document.getElementById("not_found").getElementsByTagName("p")[0].innerHTML = "Ошибка сервера!"
+		document.getElementById("not_found").getElementsByTagName("h1")[0].style.display = "none"
+		document.getElementById("not_found").style.display = "block"
+	};
+	xhr.onerror = function() {
+		document.getElementById("not_found").getElementsByTagName("p")[0].innerHTML = "Ошибка сервера!"
+		document.getElementById("not_found").getElementsByTagName("h1")[0].style.display = "none"
+		document.getElementById("not_found").style.display = "block"
+	};
+}
+function load_my_info(){
+	let name = localStorage.getItem('name');
+	if (name){
+		document.getElementById("user_name").innerHTML = name
+		document.getElementById("profile").style.display = "block"
+	}
+}
+
+
+window.onload = function() {
+	load_args()
+}
