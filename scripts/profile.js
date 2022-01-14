4f1e583f54481a80662f6866d74293ac7a9c94cd
Super Zombi <rost201541@gmail.com>
2022-01-14 07:40:04 +0200

    Update profile.js

--- scripts/profile.js
+++ scripts/profile.js
@@ -178,7 +178,7 @@ async function open_in_editor(){
 }
 function copyToClipboard(text) {
 	const elem = document.createElement('textarea');
-	elem.value = text.replace(" ", "%20");
+	elem.value = text.replaceAll(" ", "%20");
 	document.body.appendChild(elem);
 	elem.select();
 	document.execCommand('copy');
